48271ffeecaa99e655b7fc5eed6d8a270acaa5d0
Paulo Eduardo Kopzinski Mello <eduardo.geleia@gmail.com>
2015-10-04 17:33:41 -0300

    Cadastro produtos OK

---
+++ src/java/bean/ProdutoMB.java
@@ -0,0 +1,68 @@
+package bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import model.Produto;
+
+@ManagedBean(eager = true)
+@ApplicationScoped 
+public class ProdutoMB {
+    
+    private boolean novo ;
+    private List<Produto> listaProdutos;
+    private Produto clienteSelecionado;
+
+    public ProdutoMB() {
+        clienteSelecionado = new Produto();
+        listaProdutos = new ArrayList<>();
+    }
+
+    public String novoProduto() {
+        novo = true;
+        clienteSelecionado = new Produto();
+        return("/admin/formProdutos?faces-redirect=true");
+    }
+
+    public String editarProduto(Produto u) {
+        clienteSelecionado = u;
+        novo = false;
+        return("/admin/formProdutos?faces-redirect=true");
+    }
+    
+    public String salvarProduto() {
+        if(novo) {
+            listaProdutos.add(clienteSelecionado);
+            clienteSelecionado = new Produto();
+        } 
+        novo = true;
+        return("/admin/listaProdutos?faces-redirect=true");    
+    }
+
+    public void removerProduto(Produto cliente) {
+        listaProdutos.remove(cliente);
+    }
+
+    public boolean isNovo() {
+        return novo;
+    }
+    public void setNovo(boolean novo) {
+        this.novo = novo;
+    }
+    public Produto getProdutoSelecionado() {
+        return clienteSelecionado;
+    }
+    public void setProdutoSelecionado(Produto clienteSelecionado) {
+        this.clienteSelecionado = clienteSelecionado;
+    }
+
+    public List<Produto> getListaProdutos() {
+        return listaProdutos;
+    }
+    public void setListaProdutos(List<Produto> listaProdutos) {
+        this.listaProdutos = listaProdutos;
+    }    
+    
+    
+}
--- src/java/model/Produto.java
+++ src/java/model/Produto.java
@@ -15,47 +15,28 @@ public class Produto {
         this.preco = preco;
     }
 
-    /**
-     * @return the codigo
-     */
     public String getCodigo() {
         return codigo;
     }
 
-    /**
-     * @param codigo the codigo to set
-     */
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
 
-    /**
-     * @return the nome
-     */
     public String getNome() {
         return nome;
     }
 
-    /**
-     * @param nome the nome to set
-     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    /**
-     * @return the preco
-     */
     public String getPreco() {
         return preco;
     }
 
-    /**
-     * @param preco the preco to set
-     */
     public void setPreco(String preco) {
         this.preco = preco;
     }
-
     
 }   
