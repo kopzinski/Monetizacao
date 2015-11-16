f7f542844e631ea522cdec35ceaf6f07bdd7e509
Renan Padilha <renanpadilha94@hotmail.com>
2015-11-16 20:46:37 -0200

    Adicionado JPAUTIL

---
+++ src/java/util/JpaUtil.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Renan
+ */
+public class JpaUtil {
+     private static final EntityManagerFactory emf = 
+            Persistence.createEntityManagerFactory("VendaPU");
+    
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+    
+    public static void close(){
+        emf.close();
+    }
+}
